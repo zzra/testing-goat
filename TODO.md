272be2df86fcd63f048ddcd44593a834b3967c07
peter saunders <ppetersaunderss@gmail.com>
2024-03-28 18:45:14 -0400

    Redirect after post, show all items in template

---
+++ TODO.md
@@ -0,0 +1,5 @@
+* ~~don't save blank items for every request~~
+* ~~code smell: post test is too long?~~
+* pass existing items to the template somehow
+* display multiple items in the table
+* support more than one list!
--- lists/tests.py
+++ lists/tests.py
@@ -7,10 +7,22 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertContains(response, "A new list item")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/")
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):

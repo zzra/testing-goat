7ec9c67a32a7751e710eb54d15d854791352340f
peter saunders <ppetersaunderss@gmail.com>
2024-03-28 20:42:16 -0400

    removed voodoo sleeps, renamed path to PATH

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,27 +3,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
-path = "/snap/bin/geckodriver"
+
+MAX_WAIT = 5
+PATH = "/snap/bin/geckodriver"
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox(service=Service(executable_path=path))
+        self.browser = webdriver.Firefox(service=Service(executable_path=PATH))
 
     def tearDown(self):
         self.browser.quit()
     
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+
     
     def test_can_start_a_todo_list(self):
         # Edith heard about a cool new online to-do app
         # she goes to check out its homepage
         self.browser.get(self.live_server_url)
-
 
         # she notices the page title and head mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -41,8 +52,7 @@ class NewVisitorTest(LiveServerTestCase):
         # when she hits enter the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # there is still a text box inviting her to add another item
         # she enters "Use peacock feathers to make a fly"
@@ -50,13 +60,12 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # the page updates again, now shows both items on her list
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # there is still a textbox inviting her to add another item
         # she enters "User peacock feathers to make a fly" edith is methodical

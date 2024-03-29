15cbd5d121766ff533c2ddeb7687d7e7dc1af55e
peter saunders <ppetersaunderss@gmail.com>
2024-03-29 15:27:58 -0400

    moved list urls into its own, modified superlists to include the list app

--- lists/urls.py
+++ lists/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
-from lists import views as list_views
+from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('', list_views.home_page, name="home"),
-    path("lists/", include("lists.urls")),
+    path("new", views.new_list, name="new_list"),
+    path("<int:list_id>/", views.view_list, name="view_list"),
+    path("<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
 

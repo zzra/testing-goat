4916f8b083fafbf75300e4cc92375c0a7ef428b3
peter saunders <ppetersaunderss@gmail.com>
2024-03-28 23:13:42 -0400

    new urls, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -3,16 +3,9 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-        response = self.client.get("/")
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
 
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
@@ -22,11 +15,23 @@ class HomePageTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/")
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTest(TestCase):
+    def test_users_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from lists.views import home_page
+from lists import views
 
 urlpatterns = [
-    path('', home_page, name="home"),
+    path('', views.home_page, name="home"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"),
 ]

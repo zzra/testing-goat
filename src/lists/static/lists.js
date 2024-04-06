1a9fcd995d8062210314cbf189a89958425666fa
peter saunders <ppetersaunderss@gmail.com>
2024-04-06 17:19:36 -0400

    updated spacing

--- src/lists/static/lists.js
+++ src/lists/static/lists.js
@@ -1,6 +1,7 @@
 window.Superlists = {};
 window.Superlists.initialize = () => {
 	const hideErrors = () => $('.errorlist').hide();
+
 	$('input[name="text"]').on({
 		'keypress': hideErrors, 
 		'click': hideErrors
